aac965b653549936f6b1b30d2a59f61c2f9d40de
Oliver Caldwell <olliec87@gmail.com>
2016-09-27 21:55:02 +0100

    Most of the way with 1.12, Pascal's Triangle

---
+++ ex/1/12.rkt
@@ -0,0 +1,35 @@
+#lang sicp
+
+; I have to implement Pascal's Triangle.
+; I'm going to let the user specify a row and column in the triangle.
+; The solution must be recursive, it does not have to be an iterative process.
+;
+;     1
+;    1 1
+;   1 2 1
+;  1 3 3 1
+; 1 4 6 4 1
+
+(define (pascal row col)
+  (if (or (= row col) (= col 0))
+    1
+    (+ (pascal (- row 1) col)
+       (pascal (- row 1) (- 1 col)))))
+
+(define (t? e a)
+  (display (if (= e a) "✓" "✗"))
+  (display (string-append " (= " (number->string e) " " (number->string a) ")\n")))
+
+(t? 1 (pascal 0 0))
+
+(t? 1 (pascal 1 0))
+(t? 1 (pascal 1 1))
+
+(t? 1 (pascal 2 0))
+(t? 2 (pascal 2 1))
+(t? 1 (pascal 2 2))
+
+(t? 1 (pascal 3 0))
+(t? 3 (pascal 3 1))
+(t? 3 (pascal 3 2))
+(t? 1 (pascal 3 3))
