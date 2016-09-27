76055f820adc329aca7a69014c6c6e2d61f86e83
Oliver Caldwell <olliec87@gmail.com>
2016-09-27 22:31:45 +0100

    Write up question for 1.13

---
+++ ex/1/13.rkt
@@ -0,0 +1,8 @@
+#lang sicp
+
+; So this one scares me a little (a lot).
+; Q: Prove that Fib(n) is the closest integer to Φ^n/(sqrt 5), where Φ = (1 + (sqrt 5))/2
+; Hint: Let ψ = (1 - (sqrt 5))/2
+; Use induction to prove that Fib(n) = (Φ^n - ψ^n)/(sqrt 5)
+;
+; Well then. Time to use that math thing I've been hearing about.
