7806424038fcd3828ba89f33b2895dea31b0f013
Oliver Caldwell <olliec87@gmail.com>
2016-09-28 17:57:07 +0100

    Just a little note on the article I read on this
    Read it over lunch, thought I'd mention it here and move on.

--- ex/1/13.rkt
+++ ex/1/13.rkt
@@ -27,15 +27,37 @@
       (fib-iter (+ a b) a (- count 1))))
 
 ; Define the rule that I'm proving.
-(define (magic! n)
-  (/ (expt phi n) (sqrt 5)))
+(define (magic-fib n)
+  (round (/ (expt phi n) (sqrt 5))))
 
-(fib 5) ; 5
-(magic! 5) ; 4.959...
-
-(fib 9) ; 34
-(magic! 9) ; 33.994...
+; Let's compare the results
+(= (fib 0) (magic-fib 0))
+(= (fib 1) (magic-fib 1))
+(= (fib 2) (magic-fib 2))
+(= (fib 5) (magic-fib 5))
+(= (fib 9) (magic-fib 9))
+(= (fib 50) (magic-fib 50))
+(= (fib 100) (magic-fib 100)) ; They're true up until here, I think because of precision.
+(= (fib 500) (magic-fib 500))
 
 ; WAT, that's actually amazing.
 
-; I will leave the proving for tomorrow because I'm going to go watch some Mr Robot now.
+; Now to define the other approach.
+(define (alt-magic-fib n)
+  (round (/ (- (expt phi n) (expt psi n)) (sqrt 5))))
+
+; And time to test the other approach.
+(= (fib 0) (alt-magic-fib 0))
+(= (fib 1) (alt-magic-fib 1))
+(= (fib 2) (alt-magic-fib 2))
+(= (fib 5) (alt-magic-fib 5))
+(= (fib 9) (alt-magic-fib 9))
+(= (fib 50) (alt-magic-fib 50))
+(= (fib 100) (alt-magic-fib 100)) ; They're true up until here, I think because of precision.
+(= (fib 500) (alt-magic-fib 500))
+
+; I think this is the same because phi and psi appear to be inverses of each other.
+; So I don't know how to prove this through induction, I just have no idea.
+; I can kind of get it through intuition, but I can't do a proper induction.
+; Bill the Lizard explained it beautifully: http://www.billthelizard.com/2009/12/sicp-exercise-113-fibonacci-and-golden.html
+; I learned a lot there, but I still couldn't do it on my own. Sorry :(
