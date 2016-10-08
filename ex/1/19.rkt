06186088b8f7501a6147578fc3f6376886379886
Oliver Caldwell <olliec87@gmail.com>
2016-10-08 15:24:03 +0100

    Added template for 1.19

---
+++ ex/1/19.rkt
@@ -0,0 +1,20 @@
+#lang sicp
+
+; I'm given a clever efficient Fibonacci procedure. I have to fill in the gaps.
+
+(define (fib n)
+  (fib-iter 1 0 0 1 n))
+
+(define (fib-iter a b p q count)
+  (cond ((= count 0) b)
+        ((even? count)
+         (fib-iter a
+                   b
+                   #| p |#
+                   #| q |#
+                   (/ count 2)))
+        (else (fib-iter (+ (* b q) (* a q) (* a p))
+                        (+ (* b p) (* a q))
+                        p
+                        q
+                        (- count 1)))))
