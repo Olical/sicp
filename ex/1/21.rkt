9d26b82b3b8d6fe4a118cf3e3b0706229c211778
Oliver Caldwell <olliec87@gmail.com>
2016-10-15 10:38:13 +0100

    1.21

---
+++ ex/1/21.rkt
@@ -0,0 +1,24 @@
+#lang sicp
+
+; Just have to find the smallest divisor for 199, 1999 and 19999 using the
+; provided procedure.
+
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+
+(define (divides? a b)
+  (= (remainder b a) 0))
+
+(define (square n)
+  (* n n))
+
+(smallest-divisor 199) ; 199
+(smallest-divisor 1999) ; 1999
+(smallest-divisor 19999) ; 7
+
+; The first two are prime because they are their own smallest divisor.
