dba3919d9bb4cc3986da5129c4367373bd862603
Oliver Caldwell <olliec87@gmail.com>
2016-11-27 11:02:18 +0000

    Better explanation of 1.5

--- exercises/1.5.scm
+++ exercises/1.5.scm
@@ -14,16 +14,11 @@
 ;; How will this behave in applicative and normal order evaluation interpreter.
 
 #|
-What should happen? I'm guessing it's a question of "where will it lock up?"
-or "when will it lock up?". It will be different depending on the style of
-interpreter we use.
-
-So Scheme uses applicative order, that means the arguments are evaluated at
-invocation time. So it hits the (p) and chokes. In normal order, it will only
-use the arguments if it needs to.
-
-In my head I see this as top down / bottom up. Scheme is top down so it must
-evaluate the arguments as early as possible.
-
-I guess both methods have good and bad things about them. Both are interesting.
+Scheme is applicative order, so things are evaluated at application
+time (when you execute a procedure). Normal order will evaluate
+something when it is actually required. So for scheme, this locks up
+before test is even called because (p) is an argument that loops
+forever and it must be evaluated before test. In normal order (which)
+is almost lazy in a way the first condition of test will match and it
+will return 0. It will not execute p since the if branch did not touch it.
 |#
