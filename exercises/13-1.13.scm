793fbf02e02f76c5f9885c6cd5e5ee7c569657c7
Oliver Caldwell <olliec87@gmail.com>
2017-05-05 06:50:36 +0100

    1.13, I skipped it because math

---
+++ exercises/13-1.13.scm
@@ -0,0 +1,8 @@
+;; Q: Prove fib(n) is the closest integer to ϕ^n/sqrt(5).
+
+;; I don't know how to prove anything, so, yeah.
+;; Unless I literally copy this answer from somewhere else, I don't even know where to begin.
+
+;; I'll just take their word for it. (also I wrote a program to do this once and saw it work for lots of numbers...)
+
+;; ¯\_(ツ)_/¯
