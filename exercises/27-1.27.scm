3bf2c1ae70ee5b95d0e16aa790ac76bfaa9112dc
Oliver Caldwell <olliec87@gmail.com>
2017-05-16 21:49:59 +0100

    Prep for 1.27

---
+++ exercises/27-1.27.scm
@@ -0,0 +1,7 @@
+;; Q: Write a procedure that demonstrates Carmichael numbers.
+;; Those that fool the Fermat test.
+;; Take an integer n, test if a^n is congruent to a modulo n for every a < n.
+
+;; Test on 561, 1105, 1729, 2465, 2821 and 6601.
+
+;; Two numbers are said to be "congruent modulo n" if they both have the same remainder when divided by n.
