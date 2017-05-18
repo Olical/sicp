6517bab9a22b7dd8bf04ce494be7206fc1388e1e
Oliver Caldwell <olliec87@gmail.com>
2017-05-18 07:45:54 +0100

    1.28

--- exercises/28-1.28.scm
+++ exercises/28-1.28.scm
@@ -6,3 +6,30 @@
 ;; If we find one of those, we know it's not prime.
 ;; Modify expmod to signal if it discovers a nontrivial square root. Use it to implement Miller-Rabin based on the Fermat test.
 ;; Maybe return 0 from expmod if it hits a nontrivial square root.
+
+(define (square-check x m)
+  (if (and (not (or (= x 1) (= x (- m 1))))
+           (= (remainder (* x x) m) 1))
+      0
+      (remainder (* x x) m)))
+
+(define (expmod base exp m)
+  (cond ((= exp 0) 1)
+        ((even? exp)
+         (square-check (expmod base (/ exp 2) m) m))
+        (else
+         (remainder (* base (expmod base (- exp 1) m))
+                    m))))
+
+(define (miller-rabin-test n)
+  (define (try-it a)
+    (= (expmod a (- n 1) n) 1))
+  (try-it (+ 2 (random (- n 2)))))
+
+(miller-rabin-test 7)
+(miller-rabin-test 561)
+(miller-rabin-test 1105)
+(miller-rabin-test 1729)
+(miller-rabin-test 2465)
+(miller-rabin-test 2821)
+(miller-rabin-test 6601)
