15f7590e53780e463fff446ac77db3180d49d954
Oliver Caldwell <olliec87@gmail.com>
2018-04-17 00:01:17 +0100

    1.3
    I think they wanted me to use cond and do it all by hand but FP ALL THE
    THINGS.

---
+++ exercises/0002-1.3.scm
@@ -0,0 +1,13 @@
+;; Define a procedure that takes three numbers as arguments
+;; and returns the sum of the squares of the two larger
+;; numbers.
+
+(begin
+  (define (square n)
+    (* n n))
+
+  (define (sum-square-largest-2 . xs)
+    (apply + (map square (take (sort xs >) 2))))
+
+  (sum-square-largest-2 10 5 20))
+;; =>
